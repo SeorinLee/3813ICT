2082e74f089cac5490404124bf57f6e5d77f46d3
SeorinLee <yms9074@gmail.com>
2024-07-25 10:11:25 +1000

    Added adding function

---
+++ myFirstGit/index.js
@@ -0,0 +1,11 @@
+// index.js
+console.log("hello world");
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log("Sum of 5 + 10 is:", add(5, 10));
+
+// 해결된 부분
+// console.log("I caused a merge conflict"); // 충돌 발생 부분
